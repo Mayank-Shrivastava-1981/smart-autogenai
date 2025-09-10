890e87fae227827ef685cb8e2a7b03048129e639
Mayank-Shrivastava-1981 <shrivastavamas@gmail.com>
2025-09-11 04:14:53 +0530

    Updated headless=new

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def fetch_page(state):
         html = state.get("html")
 
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--log-level=3")
